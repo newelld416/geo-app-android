37a3e5c7978b24353e884094df339d34cbfa8501
Daniel Newell <daniel.newell@amway.com>
2017-05-30 21:18:09 -0400

    HW 8 Part 2

--- GeoApp/app/src/main/java/com/example/aiute40/geoapp/HistoryActivity.java
+++ GeoApp/app/src/main/java/com/example/aiute40/geoapp/HistoryActivity.java
@@ -2,13 +2,8 @@ package com.example.aiute40.geoapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.aiute40.geoapp.history.HistoryContent;
 
@@ -20,15 +15,6 @@ public class HistoryActivity extends AppCompatActivity implements HistoryFragmen
         setContentView(R.layout.activity_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
